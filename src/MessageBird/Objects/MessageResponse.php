ecb6cd0150cf6b6afaf64d3ac32e5216f7d3945f
Wouter de Berg <wouterdeberg@gmail.com>
2022-11-24 16:36:18 +0100

    Updated message response

--- src/MessageBird/Objects/MessageResponse.php
+++ src/MessageBird/Objects/MessageResponse.php
@@ -2,13 +2,13 @@
 
 namespace MessageBird\Objects;
 
+use MessageBird\Objects\Conversation\Content;
 use stdClass;
 
 /**
- * Class Message
+ * Class Message.
  *
  * @property int $protocolId
- * @package MessageBird\Objects
  */
 class MessageResponse extends Base
 {
@@ -21,27 +21,26 @@ class MessageResponse extends Base
     public $id;
 
     /**
-     * The URL of the created object.
+     * An unique random ID which is created on the MessageBird
+     * platform and is returned upon creation of the conversation object.
      *
      * @var string
      */
-    public $href;
+    public $conversationId;
 
     /**
-     * Tells you if the message is sent or received.
-     * mt: mobile terminated (sent to mobile)
-     * mo: mobile originated (received from mobile)
-     *
-     * @var string
+     * The platform the message is sent over.
      */
-    public $direction;
+    public $platform;
 
     /**
-     * The type of message. Values can be: sms, binary, premium, or flash
+     * The receiver of the message. This can be a telephone number
+     * (including country code) or an alphanumeric string. In case
+     * of an alphanumeric string, the maximum length is 11 characters.
      *
      * @var string
      */
-    public $type;
+    public $to;
 
     /**
      * The sender of the message. This can be a telephone number
@@ -50,70 +49,51 @@ class MessageResponse extends Base
      *
      * @var string
      */
-    public $originator;
+    public $from;
 
     /**
-     * The body of the SMS message.
+     * The channel ID the message is sent over.
+     */
+    public $channelId;
+
+    /**
+     * The type of message. Values can be: sms, binary, premium, or flash.
      *
      * @var string
      */
-    public $body;
+    public $type;
 
     /**
-     * A client reference. Here you can put your own reference,
-     * like your internal reference.
+     * The content of the message.
+     */
+    public $content;
+
+    /**
+     * Tells you if the message is sent or received.
+     * mt: mobile terminated (sent to mobile)
+     * mo: mobile originated (received from mobile).
      *
      * @var string
      */
-    public $reference;
+    public $direction;
+
+    public $status;
 
     /**
-     * The amount of seconds that the message is valid.
-     * If a message is not delivered within this time,
-     * the message will be discarded.
-     *
-     * @var int
-     */
-    public $validity;
-
-    /**
-     * The SMS route that is used to send the message. This is for
-     * advanced users.
-     *
-     * @var int
-     */
-    public $gateway;
-
-    /**
-     * The datacoding used, can be plain or unicode
-     *
-     * @var string
-     */
-    public $datacoding;
-
-    /**
-     * Indicates the message type. 1 is a normal message, 0 is a flash message.
-     *
-     * @var int
-     */
-    public $mclass = 1;
-
-    /**
-     * The scheduled date and time of the message in RFC3339 format (Y-m-d\TH:i:sP)
-     *
-     * @var string|null
-     */
-    public $scheduledDatetime;
-
-    /**
-     * The date and time of the creation of the message in RFC3339 format (Y-m-d\TH:i:sP)
+     * The date and time of the creation of the message in RFC3339 format (Y-m-d\TH:i:sP).
      * @var string
      */
     public $createdDatetime;
 
     /**
+     * The date and time of the last update of the message in RFC3339 format (Y-m-d\TH:i:sP).
+     * @var string
+     */
+    public $updatedDatetime;
+
+    /**
      * @deprecated 2.2.0 No longer used by internal code, please switch to {@see self::loadFromStdclass()}
-     * 
+     *
      * @param mixed $object
      */
     public function loadFromArray($object): MessageResponse
@@ -126,6 +106,12 @@ class MessageResponse extends Base
     public function loadFromStdclass(stdClass $object): self
     {
         parent::loadFromStdclass($object);
+
+        if (property_exists($object, 'content')) {
+            $content = new Content();
+            $content->loadFromStdclass($object->content);
+            $this->content = $content;
+        }
 
         return $this;
     }
